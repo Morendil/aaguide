58b133dcfb8f2e3fbad6f357bfbe1828b17e53af
Laurent Bossavit <laurent@BigMac-2.local>
2011-12-06 09:30:24 +0100

    Implement progress dashboard

--- lib/guide.rb
+++ lib/guide.rb
@@ -24,6 +24,23 @@ class Guide < Sinatra::Base
     erb (haml (mustache :practice))
   end
 
+  get '/dash.html' do
+    opts = {}
+    opts[:parts] = roadmap.all_by_type.map do |part|
+      {:group => part[:group]+"s",
+       :full => part[:values].select{|v|v["full"]}.length,
+       :total => part[:values].length}
+    end
+    opts[:parts] << {:group => "Total",
+      :full => roadmap.all.select{|v|v["full"]}.length,
+      :total => roadmap.all.length}
+    opts[:parts].each do |each|
+      each[:pct] = sprintf( "%0.0f",each[:full]*100.0/each[:total])
+    end
+p opts
+    erb (haml (mustache :dash, {}, opts))
+  end
+
   get '/subway.html' do
     erb :subway, :layout=>false
   end
